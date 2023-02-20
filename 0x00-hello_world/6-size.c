9afaef35575568bdd2acfb04c6bb664df8caa302
cokumu <okumucarolyne359@gmail.com>
2023-02-20 12:11:19 -0800

    100-intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 Success
+ *
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of", a char : %lu byte(s)\n", (unsigned long)size of (a));
+	printf("size of", an int : %lu byte(s)\n", (unsigned long)size of (b));
+	printf("size of", a long int : %lu byte(s)\n", (unsigned long)size of (c));
+	printf("size of", a long long : %lu byte(s)\n", (unsigned long)size of (d));
+	printf("size of", a float : %lu byte(s)\n", (unsigned long)size of (f));
+	return (0);
+}
